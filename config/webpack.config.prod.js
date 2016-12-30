d2b444e0b347d0049a2d8e9fc836456ce2c36b85
Phuoc <phuoc317049@gmail.com>
2016-12-30 20:54:05 +0800

    Debugged config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -33,7 +33,14 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'API_KEY': JSON.stringify(process.env.API_KEY),
+        'AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+        'DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+        'STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+        'MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID)
+      }
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.DedupePlugin(),
