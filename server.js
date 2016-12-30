0b3ce50957f4470bfbea9ad4808db9708ca6fae0
Phuoc <phuoc317049@gmail.com>
2016-12-30 13:34:30 +0800

    Ready for heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
   }
 })
 
-app.use(express.static('public'))
+app.use(express.static('build'))
 
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT)
